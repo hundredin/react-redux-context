cadb7e6931062db956e46493f6cc9f2a014aa891
Seungbeom Baek <hundredin9@gmail.com>
2020-09-13 16:27:00 +0900

    2. context 를 사용하는 세가지 방법

--- src/ContextExample.jsx
+++ src/ContextExample.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useContext} from 'react';
+
+const ThemeContext = React.createContext('light');
 
 function ContextExample() {
   return (
-    <Toolbar theme="red"/>
+    <ThemeContext.Provider value="blue">
+      <Toolbar/>
+    </ThemeContext.Provider>
   );
 }
-function Toolbar(props) {
+function Toolbar() {
   return (
     <div>
-      <ThemedButton theme={props.theme}/>
+      <ThemedButton/>
     </div>
   )
 }
 
+/**
+ * context 를 사용하는 세가지 방법
+ * 1. class type Component 에서 contextType 에 context 할당
+ * 2. Consumer 사용
+ * 3. useContext hook사용
+ */
 class ThemedButton extends React.Component {
+  static contextType = ThemeContext
   render() {
-    return <Button theme={this.props.theme}/>
+    return <Button theme={this.context}/>
   }
 }
+
+// function ThemedButton() {
+//   return (
+//     <ThemeContext.Consumer>
+//       {value => <Button theme={value}/>}
+//     </ThemeContext.Consumer>
+//   )
+// }
+
+// function ThemedButton() {
+//   const context = useContext(ThemeContext)
+  
+//   return (
+//     <Button theme={context}/>
+//   )
+// }
 
 function Button(props) {
   const { theme } = props
